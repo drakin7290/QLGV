df4fc8c4630fbf503a1aacd960dbd8c6d4d1db5e
okay7290 <okay7290@gmail.com>
2024-05-17 07:25:13 +0700

    fix

---
+++ QLGV/DTO/BaoCao_DTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class BaoCao_DTO
+    {
+        private string _id;
+
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+    }
+}
